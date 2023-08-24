b46b536fd6d2c12a245f62b1ea4b0d205d65cea8
Haley May <haymon24u@gmail.com>
2023-08-23 20:40:25 -0700

    created ternary operator for printing cocktail slides in cocktailSlide component

--- cocktail-library/client/src/components/CocktailSlide.js
+++ cocktail-library/client/src/components/CocktailSlide.js
@@ -14,27 +14,57 @@ const CocktailSlide = () => {
   const newList = shuffle(cocktailsData);
   console.log(newList);
 
-  
+//   const buttons = document.querySelectorAll("[data-carousel-button]");
+//   buttons.forEach((button) => {
+//     button.addEventListener("click", () => {
+//       const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+//       const slides = button
+//         .closest("[data-carousel]")
+//         .querySelector("[data-slides]");
+
+//       const activeSlide = slides.querySelector("index4");
+//       let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+//       if (newIndex < 0) newIndex = slides.children.length - 1;
+//       if (newIndex >= slides.children.length) newIndex = 0;
+
+//       slides.children[newIndex].dataset.active = true;
+//     });
+//   });
+
   return (
     <>
       <section aria-label="cocktail-slide">
-        <div className="carousel">
-          <button className="carousel-button prev">&#8678;</button>
-          <button className="carousel-button next">&#8680;</button>
-          <ul>
-            {newList.map((cocktailsData) => { 
-               const i = newList.indexOf(cocktailsData)
-              return (
-                <li className={`slide index`+i}>
+        <div className="carousel" data-carousel>
+          <button className="carousel-button prev" data-carousel-button="prev">
+            &#8678;
+          </button>
+          <button className="carousel-button next" data-carousel-button="next">
+            &#8680;
+          </button>
+          <ul data-slides>
+            {newList.map((cocktailsData) => {
+              const i = newList.indexOf(cocktailsData);
+              return (newList.length -1) === i ? (
+                <li className={`slide index` + i}>
                   <Card>
                     <div className="card-body">
                       <h5 className="card-title">{cocktailsData.name}</h5>
-                      <h6 className="card-subtitle mb-2 text-muted">
-                        {i}
-                      </h6>
+                      <h6 className="card-subtitle mb-2 text-muted">{i}</h6>
                       <p className="card-text">
                         Some quick example text to build on the card title and
                         make up the bulk of the card's content.
+                      </p>
+                    </div>
+                  </Card>
+                </li>
+              ) : (
+                <li className={`slide index` + i}>
+                  <Card>
+                    <div className="card-body">
+                      <h5 className="card-title">{cocktailsData.name}</h5>
+                      <h6 className="card-subtitle mb-2 text-muted">{i}</h6>
+                      <p className="card-text">
+                       this one is the else statement
                       </p>
                     </div>
                   </Card>
